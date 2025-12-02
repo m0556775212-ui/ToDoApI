e7e4a985005b949c8bc4989a45be5e7f80b5ccfe
m0556775212-ui <m0556775212@gmail.com>
2025-12-02 23:11:07 +0200

    Update Dockerfile 4

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6142d1381754dd680282096b6ca2caaaee86cc9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69a795ef3898b5db92cc085214c674d586d55b04")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
